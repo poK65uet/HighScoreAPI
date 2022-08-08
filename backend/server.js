cabff817ac661552ae003cd0c6099e049a06b845
20020066 Phạm Quang Phong <20020066@vnu.edu.vn>
2022-08-08 09:16:06 +0700

    make it better

--- backend/server.js
+++ backend/server.js
@@ -22,5 +22,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server listening on http://localhost:${PORT}`);
 })
